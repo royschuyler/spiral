c9cbd0362f9a9fbad1ad0739068e2ef66c16f893
Thomas Schuyler <thomasschuyler@Thomass-MBP.attlocal.net>
2020-03-08 05:50:33 -0700

    end.js plotting 3, 4line curves

--- end.js
+++ end.js
@@ -7,23 +7,34 @@ var a = 1.6;
 var f1 = 1;
 var f2 = 1.1;
 var bAndDSize = 0.85;
-var endText = ''
-
-
-
-
-
-
-var baseRingStart = 30;
-var ringStart = 30;
 var aToCAdd = 15;
+var baseRingStart = 30;
+var endText = '';
+var keep = 360;
 
-var objOut = makeShape(d,n,a,f1,f2,bAndDSize,baseRingStart,ringStart,aToCAdd)
+function plotThreeRings(start){
 
-endText += plot(objOut.a.x,objOut.a.y) + '</br>';
-endText += plot(objOut.b.x,objOut.b.y) + '</br>';
-endText += plot(objOut.c.x,objOut.c.y) + '</br>';
-endText += plot(objOut.d.x,objOut.d.y) + '</br>';
+	var ringStart = start;
+
+	var objOut = makeShape(d,n,a,f1,f2,bAndDSize,baseRingStart,ringStart,aToCAdd)
+
+	endText += plot(objOut.a.x,objOut.a.y) + '</br>';
+	endText += plot(objOut.b.x,objOut.b.y) + '</br>';
+	endText += plot(objOut.c.x,objOut.c.y) + '</br>';
+	endText += plot(objOut.d.x,objOut.d.y) + '</br>';
+
+}
+
+var numberOfRings = 3;
+
+plotThreeRings((keep/numberOfRings)*1);
+plotThreeRings((keep/numberOfRings)*2);
+plotThreeRings((keep/numberOfRings)*3);
+
+
+
+
+
 
 var extra = 'blinewidth 0.5 all' + '</br>' + 'drawframe no' + '</br>' + 'asetticks x no' + '</br>' + 'asetticks y no' + '</br>' + 'asetminticks x no' + '</br>' + 'asetminticks y no' + '</br>' +'framewidth 0' + '</br>' + 'bstyle yes no no no no no no yes no no 0' + '</br>' + 'margins 0 0 0 0' + '</br>' + 'range x '  + -plotSize + ' ' + plotSize + '</br>' + 'range y ' + -plotSize + ' ' + plotSize;
 
