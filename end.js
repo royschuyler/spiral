7b1525b7c0276419309106f6b98cd3088d4f0237
Thomas Schuyler <thomasschuyler@Thomass-MBP.attlocal.net>
2020-03-08 11:54:53 -0700

    cleanup

--- end.js
+++ end.js
@@ -1,18 +1,18 @@
 console.log('hello world')
 var finalCount = 0;
 var plotSize = 2;
-var d = 100;
-var n = 2;
+var d = 6;
+var n = .7;
 var a = 1.6;
 var f1 = 1;
 var f2 = 1.1;
-var bAndDSize = 0.85;
-var aToCAdd = 15;
-var baseRingStart = 30;
+var bAndDSize = 0.75;
+var aToCAdd = 25;
+var baseRingStart = 15;
 var endText = '';
 var keep = 360;
 
-function plotThreeRings(start){
+function plotRing(start){
 
 	var ringStart = start;
 
@@ -27,16 +27,13 @@ function plotThreeRings(start){
 
 var numberOfRings = 3;
 
-plotThreeRings((keep/numberOfRings)*1);
-plotThreeRings((keep/numberOfRings)*2);
-plotThreeRings((keep/numberOfRings)*3);
+plotRing((keep/numberOfRings)*1);
+plotRing((keep/numberOfRings)*2);
+plotRing((keep/numberOfRings)*3);
 
 
 
-
-
-
-var extra = 'blinewidth 0.5 all' + '</br>' + 'drawframe no' + '</br>' + 'asetticks x no' + '</br>' + 'asetticks y no' + '</br>' + 'asetminticks x no' + '</br>' + 'asetminticks y no' + '</br>' +'framewidth 0' + '</br>' + 'bstyle yes no no no no no no yes no no 0' + '</br>' + 'margins 0 0 0 0' + '</br>' + 'range x '  + -plotSize + ' ' + plotSize + '</br>' + 'range y ' + -plotSize + ' ' + plotSize;
+var extra = 'blinewidth 0.5 all' + '</br>' + 'drawframe no' + '</br>' + 'asetticks x no' + '</br>' + 'asetticks y no' + '</br>' + 'asetminticks x no' + '</br>' + 'asetminticks y no' + '</br>' +'framewidth 0' + '</br>' + 'bstyle yes no no no no no no yes no no 0' + '</br>' + 'margins 0 0 0 0' + '</br>' + 'range x '  + -plotSize + ' ' + plotSize+ '</br>' + 'range y ' + -plotSize + ' ' + plotSize;
 
 var finish = endText + '</br>' + extra
 
